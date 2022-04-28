77a875371da37d6a694c96f0fa97c7fdc1535f5b
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2022-04-28 15:09:26 +0900

    Counter.js

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
 
 function Counter() {
-    const [number, setNumber] = useState(0);
+    const [number, dispatch] = useReducer(reducer, 0);
+
     const onIncrease = () => {
-        setNumber(thisNum => thisNum + 1);
-    }
+        dispatch({ type : 'INCREMENT' });
+    };
+    
     const onDecrease = () => {
-        setNumber(thisNum => thisNum - 1);
-    }
-    return (
+        dispatch({ type : 'DECREMENT'});
+    };
+
+    return(
         <div>
             <h1>{number}</h1>
             <button onClick={onIncrease}>+1</button>
