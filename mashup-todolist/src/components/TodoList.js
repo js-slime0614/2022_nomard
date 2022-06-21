d4444dbeaef8d7a9200413031ee5ad498e38ae31
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2022-06-21 16:43:11 +0900

    TodoCreate

--- mashup-todolist/src/components/TodoList.js
+++ mashup-todolist/src/components/TodoList.js
@@ -9,7 +9,12 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-    return <TodoListBlock>TodoList</TodoListBlock>;
+    return <TodoListBlock>
+        <TodoItem text="프로젝트 생성하기" done={true} />
+        <TodoItem text="컴포넌트 스타일링 하기" done={true} />
+        <TodoItem text="Context 만들기" done={false} />
+        <TodoItem text="기능 구현하기" done={false} />
+    </TodoListBlock>;
 }
 
 export default TodoList;
